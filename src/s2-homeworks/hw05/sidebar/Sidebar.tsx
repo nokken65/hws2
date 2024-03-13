e1e02663dd9b99ced0f4075c854a43f9e99cbbe3
nokken65 <unknown65182@gmail.com>
2024-03-13 20:35:25 +0300

    feat: done hw5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -42,7 +42,7 @@ export const Sidebar: React.FC<PropsType> = ({ open, handleClose }) => {
             id={'hw5-pre-junior-link'}
             to={PATH.PRE_JUNIOR}
             onClick={handleClose}
-            // className={...} // делает студент
+            className={({ isActive }) => (isActive ? s.active : '')} // делает студент
           >
             Pre-junior
           </NavLink>
@@ -50,7 +50,7 @@ export const Sidebar: React.FC<PropsType> = ({ open, handleClose }) => {
             id={'hw5-junior-link'}
             to={PATH.JUNIOR}
             onClick={handleClose}
-            // className={...} // делает студент
+            className={({ isActive }) => (isActive ? s.active : '')} // делает студент
           >
             Junior
           </NavLink>
@@ -58,7 +58,7 @@ export const Sidebar: React.FC<PropsType> = ({ open, handleClose }) => {
             id={'hw5-junior-plus-link'}
             to={PATH.JUNIOR_PLUS}
             onClick={handleClose}
-            // className={...} // делает студент
+            className={({ isActive }) => (isActive ? s.active : '')} // делает студент
           >
             Junior Plus
           </NavLink>

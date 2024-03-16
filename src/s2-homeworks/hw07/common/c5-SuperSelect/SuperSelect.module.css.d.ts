f633454be489a49724e11ec66eae9ca942e6c80f
nokken65 <unknown65182@gmail.com>
2024-03-16 23:13:23 +0300

    feat: done hw7

---
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.module.css.d.ts
@@ -0,0 +1,5 @@
+declare const classNames: {
+  readonly select: 'select'
+  readonly option: 'option'
+}
+export = classNames
---
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.module.css.d.ts
@@ -0,0 +1,7 @@
+declare const classNames: {
+  readonly radio: 'radio'
+  readonly label: 'label'
+  readonly options: 'options'
+  readonly span: 'span'
+}
+export = classNames

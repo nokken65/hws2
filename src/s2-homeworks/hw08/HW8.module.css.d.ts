9b7d05f91c7a575216ff105a451bbe86a5dde4f2
nokken65 <unknown65182@gmail.com>
2024-03-24 22:17:08 +0300

    feat: hw8 done

---
+++ src/s2-homeworks/hw08/HW8.module.css.d.ts
@@ -0,0 +1,10 @@
+declare const classNames: {
+  readonly item: 'item'
+  readonly users: 'users'
+  readonly buttonsContainer: 'buttonsContainer'
+  readonly container: 'container'
+  readonly thead: 'thead'
+  readonly nameCol: 'nameCol'
+  readonly ageCol: 'ageCol'
+}
+export = classNames
